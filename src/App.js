7cbe997fb13ca6dbb55ee4092915aeafbd011bee
Jakub Kopras <g00403547@atu.ie>
2024-08-23 21:39:22 +0100

    Added imports to app.js will now start working on app functionality and connecting evrything to work together.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { useState, useEffect } from 'react';
+import WordDisplay from './components/wordDisplay';
+import TypingInput from './components/TypingInput';
+import Timer from './components/Timer';
+import ParagraphDisplay from './components/ParagraphDisplay';
+import ParagraphMetrics from './components/ParagraphMetrics';
+import { WORDS, PARAGRAPHS } from './Data/wordList';
+import './styles/App.css';
